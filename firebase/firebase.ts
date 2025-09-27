54339a7a1625124e77d0d7007612f2d2eee14354
Hashim-Zafar <hashimzafar420@gmail.com>
2025-09-27 21:14:12 +0500

    Fixed Security Issue

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyD-kSmc--J1GrT-O47f8i3mlFrrMHkK-6k",
-  authDomain: "prepwise-5d7a8.firebaseapp.com",
-  projectId: "prepwise-5d7a8",
-  storageBucket: "prepwise-5d7a8.firebasestorage.app",
-  messagingSenderId: "419009945089",
-  appId: "1:419009945089:web:c0752c279825875f2bea00",
-  measurementId: "G-W8JNYTPV08",
+  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSANGING_SENDER_ID,
+  appId: process.env.PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.PUBLIC_FIREBASE_MEASURMENT_ID,
 };
 
 // Initialize Firebase
